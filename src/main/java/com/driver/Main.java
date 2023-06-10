9c556d4ec0d4dd184dfbecae3d9e56988ca6f1c6
sanyam raj <sanyamraj007@gmail.com>
2023-06-10 21:34:14 +0530

    first try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
-
+import java.util.*;
 public class Main {
+    public static class Product{
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(5 , 6);
+        p.product(5 , 6 ,2);
+        p.product(2.0,3.0);
+    }
 
 }
